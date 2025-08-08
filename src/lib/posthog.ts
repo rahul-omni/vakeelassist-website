a39fefd7d9b097fab024cba66f95782c9eba2e85
rawatkash <akash.rawat@arthmate.co.in>
2025-08-08 11:44:59 +0530

    capture pageviews

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -7,10 +7,10 @@ export const initPosthog = () => {
     !window.location.hostname.includes('127.0.0.1')) {
     posthog.init(process.env.NEXT_PUBLIC_POST_HOG_API_TOKEN || "", {
       api_host: process.env.NEXT_PUBLIC_POST_HOG_API_HOST,
-      autocapture: false,                    // ❌ disables auto events like clicks, inputs
-      disable_session_recording: true,      // ❌ disables session replays
-      capture_pageview: false,              // ❌ disables automatic pageview capture
-      disable_compression: true,            // 🛑 avoids gzip-js calls
+      autocapture: false,
+      disable_session_recording: true,
+      capture_pageview: true,       
+      disable_compression: true,            
       advanced_disable_decide: true,  
     })
   }
