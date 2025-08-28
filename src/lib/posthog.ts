27a11124b07babaa4f69282cfddaf531eed20494
rawatkash <akash.rawat@arthmate.co.in>
2025-08-28 11:50:59 +0530

    enable recordings

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -8,10 +8,10 @@ export const initPosthog = () => {
     posthog.init(process.env.NEXT_PUBLIC_POST_HOG_API_TOKEN || "", {
       api_host: process.env.NEXT_PUBLIC_POST_HOG_API_HOST,
       autocapture: false,
-      disable_session_recording: false,
-      capture_pageview: true,       
-      disable_compression: true,            
-      advanced_disable_decide: true,  
+      capture_pageview: true,
+      disable_compression: false,   // you can leave this true/false, not critical
+      disable_session_recording: false, // enable session recording
+      advanced_disable_decide: false,
     })
   }
 }
